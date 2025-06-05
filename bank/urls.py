9bfe86924790e2809be0da0db91024654a58d308
Ucer <ucer@Ucers-MacBook-Air.local>
2025-06-05 11:57:47 +0500

    face

--- bank/urls.py
+++ bank/urls.py
@@ -1,13 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from user.views import MyTokenObtainPairView,MailItemUpdateStatus,MailItemAllListView,BatchStatisticsAPIView,CheckMailItemAPIView,MailItemStatsAPIView,ReceivedDateMonthCountView,DeliveredMailItemListView
+from user.views import FaceRecognitionAPIView,MyTokenObtainPairView,MailItemUpdateStatus,MailItemAllListView,BatchStatisticsAPIView,CheckMailItemAPIView,MailItemStatsAPIView,ReceivedDateMonthCountView,DeliveredMailItemListView
 from rest_framework_simplejwt.views import TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('management/admin/', admin.site.urls),
-    #path('api/v1/face-recognition/', FaceRecognitionAPIView.as_view(), name='face-recognition'),
+    path('api/v1/face-recognition/', FaceRecognitionAPIView.as_view(), name='face-recognition'),
     path('api/v1/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/order/',MailItemUpdateStatus.as_view(), name="update-status"),
